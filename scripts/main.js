070f3b3cfe63469b9f4ae94ad4d5e2ea7adb6414
Roman Shmelev <r-shmelev@mail.ru>
2020-08-08 14:03:13 +0300

    lesson02

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,14 @@ let mission = 1000000;
 let period = 6;
 let budgetDay = 50000;
 
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(period + ' месяцев ' + mission);
 
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+console.log(Math.floor (budgetDay / 30));
 
