cf331601e313fa3cbbac3fea5be14bdbafefed2c
Roman Shmelev <r-shmelev@mail.ru>
2020-09-03 16:35:43 +0300

    lesson18_full

--- scripts/main.js
+++ scripts/main.js
@@ -33,9 +33,60 @@ window.addEventListener('DOMContentLoaded', function() {
         } 
         let init = setInterval(uppdateClock, 1000);
 	}
-    
     countTimer('2 september 2020');
+
+
+//Пишем меню
+
+const toggleMenu = () => {
+    //Получаем элементы со страницы
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+        menu.classList.toggle('active-menu');
+    };   
+        
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+
+};
+toggleMenu();
+
+//Всплывашка
+
+const togglePopUp = () => {
+    //Поулчаем элементы
+    const popup = document.querySelector('.popup'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close'),
+        popupContent =document.querySelector('.popup-content');
+
+    popupBtn.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            popup.style.display = 'block';
+            if(window.screen.availWidth > 768) modalAnimation();
+        });
+    });
+    popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+    //Анимация
+    const modalAnimation = () => {
+        let count = 0;
+        popupContent.style.left = 0;
+        function newAnimation () {
+            popupContent.style.left = count + `%`;
+            count++;
+            if(count <= 38){
+                requestAnimationFrame(newAnimation);
+            }
+        };
+        newAnimation();
+    };
+};
+togglePopUp();
 });
-
-
-
