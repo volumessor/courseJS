98823615099eedc8dde197ef376a7737500b84e0
Roman Shmelev <r-shmelev@mail.ru>
2020-09-05 11:37:59 +0300

    lesson20

--- scripts/main.js
+++ scripts/main.js
@@ -50,19 +50,17 @@ const toggleMenu = () => {
     };   
     
     //Новый обработчик
-    const containerParent = document.body;
-    containerParent.addEventListener('click', (event) => {
+    document.addEventListener(`click`, (event) => {
         let target = event.target;
-        if(target.closest('.menu')){
-            handlerMenu();
-        } 
-        if(target.closest('.close-btn')){
-            handlerMenu();
+        if(!menu.matches(`.active-menu`)) {
+            target = target.closest(`.menu`);
+            if(target) handlerMenu();
+        } else {
+            if (target.matches(`.close-btn`) || !target.matches(`menu`)) {
+                handlerMenu();
+            }
         }
-        if(target.closest('ul>li')){
-            handlerMenu();
-        }
-    });
+    })
 };
 toggleMenu();
 
@@ -143,4 +141,106 @@ togglePopUp();
     });
     };
     tabs();
+
+//Пишем слайдер
+
+const slider = () => {
+    const slide = document.querySelectorAll(`.portfolio-item`);
+
+    const newDot = () => {
+        const ulDots = document.querySelector(`.portfolio-dots`);
+        for(let i = 0; i < slide.length; i++) {
+            ulDots.insertAdjacentHTML(`beforeend`, `<li class="dot"></li>`);
+            if(i === 0) {
+                document.querySelector(`.dot`).classList.add(`dot-active`);
+            }
+        }
+    }
+    newDot();
+
+    //Получаем элементы со страницы
+        const dot = document.querySelectorAll(`.dot`),
+        slider = document.querySelector(`.portfolio-content`),
+        btn = document.querySelectorAll('.portfolio-btn');
+    //Определние слайда на экране
+        let currentSlide = 0;  //Номер слайда
+        let interval = 0;
+    
+
+const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+};
+const nextSlide = (elem, index, strClass) => {
+    elem[index].classList.add(strClass);
+};
+    //Автоматическая прокрутка
+    const autoPlaySlider = () => {
+        prevSlide(slide, currentSlide, 'portfolio-item-active');
+        prevSlide(dot, currentSlide, 'dot-active');
+        currentSlide++;
+        if(currentSlide >= slide.length){
+            currentSlide = 0;
+        }
+        nextSlide(slide, currentSlide, 'portfolio-item-active');
+        nextSlide(dot, currentSlide, 'dot-active');
+    };
+    //Запуск слайдера
+    const startSlide  = (time = 3000) => {
+        interval = setInterval(autoPlaySlider, time);
+    };
+    //Остановка слайдера
+    const stopSlide  = () => {
+        clearInterval(interval);
+    };
+
+    slider.addEventListener('click', (event) =>{
+        event.preventDefault();
+        let target = event.target;
+
+        if(!target.matches('.portfolio-btn, .dot')){
+                    return;
+        }
+
+        prevSlide(slide, currentSlide, 'portfolio-item-active');
+        prevSlide(dot, currentSlide, 'dot-active');     
+        
+        if(target.matches('#arrow-right')){
+            currentSlide++;
+        } else if(target.matches('#arrow-left')){
+            currentSlide--;
+        } else if(target.matches('.dot')){
+            dot.forEach((elem, index) => {
+                if(elem === target){
+                    currentSlide = index;
+                }
+            })  
+        }
+        
+        if(currentSlide >= slide.length){
+            currentSlide = 0;
+        }
+
+        if(currentSlide < 0){
+            currentSlide = slide.length -1;
+        }
+        nextSlide(slide, currentSlide, 'portfolio-item-active');
+        nextSlide(dot, currentSlide, 'dot-active');
+    });
+
+    slider.addEventListener('mouseover', (event) =>{
+        if(event.target.matches('.portfolio-btn') ||
+        event.target.matches('.dot')){
+            stopSlide();
+        }
+    });
+    slider.addEventListener('mouseout', (event) =>{
+        if(event.target.matches('.portfolio-btn') ||
+        event.target.matches('.dot')){
+            startSlide();
+        }
+    });
+
+    startSlide(1500);
+}
+slider();
 });
